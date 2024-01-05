f5ea832a5a3a676182baca87fef158249714590d
FosseyCool <fossey.cool@gmail.com>
2024-01-05 14:07:21 +0300

    Full

---
+++ pixelBot/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace pixelBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            timerForTime = new System.Windows.Forms.Timer(components);
+            textBox1 = new TextBox();
+            muteProgram = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(110, 46);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(16, 15);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 29);
+            button1.TabIndex = 1;
+            button1.Text = "СТАРТ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // timerForTime
+            // 
+            timerForTime.Enabled = true;
+            timerForTime.Tick += timerForTime_Tick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(110, 1);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(112, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // muteProgram
+            // 
+            muteProgram.Location = new Point(12, 36);
+            muteProgram.Name = "muteProgram";
+            muteProgram.Size = new Size(75, 23);
+            muteProgram.TabIndex = 4;
+            muteProgram.Text = "Mute";
+            muteProgram.UseVisualStyleBackColor = true;
+            muteProgram.Click += muteProgram_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(252, 98);
+            Controls.Add(muteProgram);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private System.Windows.Forms.Timer timerForTime;
+        private TextBox textBox1;
+        private Button muteProgram;
+    }
+}
---
+++ pixelBot/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Media;
+using System.Globalization;
+
+namespace pixelBot
+{
+    public partial class Form1 : Form
+    {
+        private int x;
+        private int y;
+        private int w;
+        private int h;
+        private Bitmap bitmap1;
+        private Point? pt;
+
+        private SoundPlayer player;
+        // private Bitmap bitmap2;
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowTransparency = true;
+            this.BackColor = Color.AliceBlue;
+            this.TransparencyKey = this.BackColor;
+            player = new SoundPlayer(@"Sound.wav");
+        }
+
+        private void MakeScreen()
+        {
+            Point location = pictureBox1.PointToScreen(Point.Empty);
+            x = location.X + 5;
+            y = location.Y + 5;
+            w = pictureBox1.Width - 5;
+            h = pictureBox1.Height - 5;
+
+            bitmap1 = new Bitmap(w, h);
+
+            using (Graphics g = Graphics.FromImage(bitmap1))
+            {
+                g.CopyFromScreen(x + 1, y, 10, 0, new Size(w, h - 2));
+            }
+
+            bitmap1.Save("scrstart.png", System.Drawing.Imaging.ImageFormat.Png);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // MakeScreen();
+            // bitmap1.Dispose();
+
+            bitmap1 = new Bitmap(Image.FromFile("scrstart.png"));
+
+            Bitmap bmp2 = new Bitmap(w, h);
+            using (Graphics g = Graphics.FromImage(bmp2))
+            {
+                // g.CopyFromScreen(x, y, 0, 0, new Size(w, h));
+                g.CopyFromScreen(x + 1, y, 10, 0, new Size(w, h - 2));
+            }
+
+
+            pt = GetMismatchPoint(bitmap1, bmp2);
+
+            if (pt != null)
+            {
+                // MessageBox.Show("текст изменен");             
+                timer1.Enabled = false;
+                //  VolUp();
+                this.WindowState = FormWindowState.Normal;
+                this.TopMost = true;
+                player.Play();
+                player.PlayLooping();
+
+
+            }
+        }
+
+        Point? GetMismatchPoint(Bitmap bmp1, Bitmap bmp2)
+        {
+            using (var wr1 = new ImageWrapper(bmp1))
+            using (var wr2 = new ImageWrapper(bmp2))
+                foreach (var p in wr1)
+                    if (wr1[p] != wr2[p])
+                        return p;
+
+            return null;
+        }
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+        private void VolUp()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                    (IntPtr)APPCOMMAND_VOLUME_UP);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Green;
+            MakeScreen();
+            timer1.Start();
+        }
+
+        private void timerForTime_Tick(object sender, EventArgs e)
+        {
+            timerForTime.Interval = 1000;
+            timerForTime.Start();
+
+            textBox1.Text = textBox1.Text = DateTime.Now.ToString("yyyy.MM.dd, HH.mm.ss");
+        }
+
+        private void muteProgram_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            muteProgram.BackColor = Color.Blue;
+        }
+    }
+}
